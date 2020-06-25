cb00249d108b45d5d06d5486a797eeef95c06ae0
macdavel <macdavel@users.noreply.github.com>
2020-06-25 13:41:59 +0400

    Update .gitignore

--- credentials.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  consumer_key: 'UsGLKey4P81eecbuVgRVT4yRV',
-  consumer_secret: 'iQqZw2mNd82yy8eEtHZCgttPY6xeAXnJliSV6tfOkCwJdMwc8d',
-  access_token_key: '75537898-nKfHGkH8sktnNpGcwirnHIIvbOqsLEL3j6kiD9lU6',
-  access_token_secret: 'O4S2PhS4Y4XID2IcYVE354Xn56RCmee38aC6XKeHx9uLx'
-}
